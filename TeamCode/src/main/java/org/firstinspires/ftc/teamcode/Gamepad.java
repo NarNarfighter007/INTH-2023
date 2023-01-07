17ca7ab207a51ed52a7dc903cf7c327a44f15cf2
NarNarfighter007 <shajpaulme@gmail.com>
2023-01-06 21:21:28 -0500

    Successful Test autotest Program - 1/6/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
@@ -30,7 +30,17 @@ public class Gamepad extends OpMode {
         backLeft = hardwareMap.get(DcMotor.class, ("backLeft"));
         Slippy = hardwareMap.get(DcMotor.class, ("Slippy"));
         Claw = hardwareMap.get(Servo.class, ("Claw"));
+
         telemetry.addData("Servo Position:", Claw.getPosition());
+        telemetry.addData("Motor Encoder frontLeft:", frontLeft.getCurrentPosition());
+        telemetry.addData("Motor Encoder backLeft:", backLeft.getCurrentPosition());
+        telemetry.addData("Motor Encoder frontRight:", frontRight.getCurrentPosition());
+        telemetry.addData("Motor Encoder backRight:", backRight.getCurrentPosition());
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
     boolean changed = false; //Outside of loop()
     @Override
@@ -41,11 +51,19 @@ public class Gamepad extends OpMode {
         backRight.setPower((0.5)*(-gamepad1.left_stick_y + gamepad1.left_stick_x + -gamepad1.right_stick_x));
         backLeft.setPower((0.5)*(gamepad1.left_stick_y + gamepad1.left_stick_x + -gamepad1.right_stick_x));
 
-        if(gamepad1.dpad_right && !changed) {
+        telemetry.update();
+
+        telemetry.addData("Servo Position:", Claw.getPosition());
+        telemetry.addData("Motor Encoder frontLeft:", frontLeft.getCurrentPosition());
+        telemetry.addData("Motor Encoder backLeft:", backLeft.getCurrentPosition());
+        telemetry.addData("Motor Encoder frontRight:", frontRight.getCurrentPosition());
+        telemetry.addData("Motor Encoder backRight:", backRight.getCurrentPosition());
+
+   /*     if(gamepad1.dpad_right && !changed) {
             if(Claw.getPosition() == 0.3) Claw.setPosition(0);
             else Claw.setPosition(0.3);
             changed = true;
-        } else if(!gamepad1.dpad_right) changed = false;
+        } else if(!gamepad1.dpad_right) changed = false; */
 
 
         if (gamepad1.dpad_up){
@@ -60,7 +78,7 @@ public class Gamepad extends OpMode {
 
         Slippy.setPower(0);
 
-       /* if (gamepad1.dpad_right) {
+        if (gamepad1.dpad_right) {
             Claw.setPosition(0); //Closed
             telemetry.addData("Position:", 0);
         } else {
@@ -68,7 +86,7 @@ public class Gamepad extends OpMode {
             telemetry.addData("Position:", 0.3);
         }
 
-        */
+
     }
     }
 
